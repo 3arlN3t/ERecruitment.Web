c9ba9afecd69c61996c4d1706c4cb9f0cde46a0b
Otsile Earl Kole <EarlKole@gmail.com>
2025-11-07 03:27:30 +0200

    Adding some other and more few twirks on the Masterlist  designs

--- obj/Debug/net8.0/ERecruitment.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/ERecruitment.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERecruitment.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8b4daac1fdba2c888b29f89edccefa88fa9b94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+676ba0e22c09fd1c6940e0f0f70b34004dbd32c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERecruitment.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERecruitment.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
